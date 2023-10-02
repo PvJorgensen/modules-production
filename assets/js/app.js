2b2269a38148b8e76224329be6d1af7228ffa1eb
bo-nicolaisen <bni@techcollege.dk>
2023-10-02 10:13:24 +0200

    did a fetch function

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,38 @@
 
+// globals ----------------------------------------------------------------
+let myApp = document.getElementById('app');
 
-console.log(document.getElementById('app'));
+// fetch users ------------------------------------------------------------
+fetchUsers();
+
+function fetchUsers() {
+    fetch('https://dummyjson.com/users?limit=0')
+
+        .then((response) => {
+            // error check på netværk response
+            if (!response.ok) {
+                throw new Error("not ok!" + response.status);
+            }
+
+            // konverter response til json data objekt
+            let data = response.json();
+            return data;
+
+        })
+
+        .then((data) => {
+            // do stuff
+            recivedUsers(data.users);
+        })
+
+        .catch((error) => {
+            console.log(error.message);
+        });
+}
+
+
+
+function recivedUsers(myUsers) {
+    console.log(myUsers);
+}
+
